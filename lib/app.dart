ae069357f29e1039409ae3b1e78e595747d47905
arindammanna <arindam.manna@gmail.com>
2019-11-05 23:48:35 +0530

    Basic home page

--- lib/app.dart
+++ lib/app.dart
@@ -19,41 +19,47 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: PreferredSize(
-            preferredSize: Size.fromHeight(50.0), // here the desired height
+            preferredSize: Size.fromHeight(40.0), // here the desired height
             child: AppBar(
                 bottom: PreferredSize(
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(child: Icon(Icons.menu),flex: 1,),
-                      Expanded(
-                        flex: 5,
-                        child: Theme(
-                          data: Theme.of(context).copyWith(splashColor: Colors.transparent),
-                          child: Container(
-                            width: 200.0,
-                            child: TextField(
-                              autofocus: false,
-                              style: TextStyle(fontSize: 16.0, color: Color(0xFFbdc6cf)),
-                              decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-                                hintText: 'Username',
-                                contentPadding: const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white),
-                                  borderRadius: BorderRadius.circular(25.7),
-                                ),
-                                enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white),
-                                  borderRadius: BorderRadius.circular(25.7),
+                  child: Container(
+                    padding: EdgeInsets.only(bottom: 5.0),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(child: Icon(Icons.menu,color: Colors.white,size: 30,),
+                          flex: 1,
+                        ),
+                        Expanded(
+                          flex: 4,
+                          child: Theme(
+                            data: Theme.of(context).copyWith(splashColor: Colors.transparent),
+                            child: Container(
+                              width: 100.0,
+                              child: TextField(
+                                autofocus: false,
+                                style: TextStyle(fontSize: 5.0, color: Color(0xFFbdc6cf)),
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white,
+//                                  hintText: 'Username',
+                                  contentPadding: const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                    borderRadius: BorderRadius.circular(25.7),
+                                  ),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                    borderRadius: BorderRadius.circular(25.7),
+                                  ),
+
                                 ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      Expanded(child: Icon(Icons.menu),flex: 1,),
-                    ],
+                        Expanded(child: Icon(Icons.my_location,color: Colors.white,),flex: 1,),
+                      ],
+                    ),
                   ),
                 ))),
         body: [InfoScreen(), AccountScreen(), HomeScreen(), BookScreen(), FavouriteScreen()].elementAt(_selectedIndex),
--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -1,22 +1,84 @@
-
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 import '../../common/app_drawer.dart';
-
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-//        appBar: AppBar(
-//          title: const Text('Welcome to Flutter'),
-//
-//        ),
-//        drawer: AppDrawer(),
         body: Column(
-          children: <Widget>[
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 15.0,bottom: 7.0),
+                  child: CarouselSlider(
+                    items: [
+                      'http://pic3.16pic.com/00/55/42/16pic_5542988_b.jpg',
+                      'http://photo.16pic.com/00/38/88/16pic_3888084_b.jpg',
+                      'http://pic3.16pic.com/00/55/42/16pic_5542988_b.jpg',
+                      'http://photo.16pic.com/00/38/88/16pic_3888084_b.jpg'
+                    ].map((i) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin: EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: BoxDecoration(color: Colors.white,),
+                              child: GestureDetector(
+                                  child: ClipRRect(
+                                      borderRadius: new BorderRadius.circular(15.0),
+                                      child:Image.network(i, fit: BoxFit.fill,)),
+                                  onTap: () {
 
+                                  }));
+                        },
+                      );
+                    }).toList(),
+                  ),
+                )
+            ),
+            Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          FlatButton(
+                          onPressed: () => {},
+                          color: Colors.orange,
+                          padding: EdgeInsets.all(10.0),
+                          child: Row( // Replace with a Row for horizontal icon + text
+                            children: <Widget>[
+                              CircleAvatar(child: Image.asset("assets/images/car-wash.jpg",width: 1,)),
+                              Text("Add")
+                            ],
+                          ),
+                        ),],
+                      )
+                    ],
+                  ),
+                )
+            ),
+            Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(color: Colors.blue,),
+                )
+            ),
+            Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(color: Colors.blue,),
+                )
+            ),
           ],
-        ));
+    ));
   }
 }
